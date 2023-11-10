0d1f33db196531881f11f199f9ab51710f82f126
Manibandaru <Shankar.bandaru777@gmail.com>
2023-11-10 16:53:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+age=int(input("any number"))
+def life_of_mani(age):
+    if age in range(0,15):
+        print("priyanka")
+    elif age in range(16,20):
+        print("praneetha alias puppy")
+    elif age in range(21,29):
+        print("so many")
+    else:
+        print("kowsalya")
+    return True
+print(life_of_mani(age))
---
+++ practice_2.py
@@ -0,0 +1,11 @@
+strr=input("enter any stringg")
+count=0
+
+
+for i in strr:
+    if i not in dictt:
+        count=1
+    else:
+        count+=1
+
+print(i,count)
