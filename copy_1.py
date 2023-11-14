e35dc8055d54ecad94100c1ede3e2d8f05356109
kowsalya <kowsalya@gmail.com>
2023-11-14 17:02:34 +0530

    generator_iter

---
+++ copy_1.py
@@ -0,0 +1,16 @@
+import copy
+
+a= 2+3
+# shallow copy
+list_1 = [1, 2, [3, 5], 4]
+list_2 = copy.copy(list_1)
+# list_2[3] = 7
+# list_2[2].append(6)
+# print(list_2)
+# print(list_1)
+# # deep copy
+# list_3 = copy.deepcopy(list_1)
+# list_3[3] = 8
+# list_3[2].append(7)
+# print(list_3)
+# print(list_1)
---
+++ generator.py
@@ -0,0 +1,11 @@
+# generator
+
+def fabnocci(n):
+    a,b=0,1
+    if n>0:
+        for i in range(n):
+            yield a
+            a,b = b,a + b
+generator=fabnocci(10)
+print(next(generator))
+print(next(generator))
